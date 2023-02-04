f5d049f6789b3d8e8180c25ba67cdf64aa0c645b
Alexey Khaneft <a.khaneft@gmail.com>
2023-02-04 22:36:45 +0100

    improve changing ages

--- main/constants.lua
+++ main/constants.lua
@@ -1,8 +1,22 @@
 local OBJECT = {}
 OBJECT.CURRENT_AGE = "AGE_1"
 OBJECT.AGES = { 
-	AGE_1 = { next = "AGE_2", minimal_point = 500, damage_object = "#stone", point_object = "#nokia"},
-	AGE_2 = { next = "", minimal_point = 1500, damage_object = "#stone", point_object = "#bones"}
+	AGE_1 = { 
+		next = "AGE_2",
+		minimal_point = 500, 
+		damage_object = "#stone", 
+		point_object = "#nokia",
+		stop_ground = "main:/ground/ground0#ground",
+		start_ground = "main:/ground2/ground0#ground"
+	},
+	AGE_2 = { 
+		next = "", 
+		minimal_point = 1500, 
+		damage_object = "#stone", 
+		point_object = "#bones",
+		stop_ground = "",
+		start_ground = ""
+	}
 }
 
 OBJECT.speed = 150
