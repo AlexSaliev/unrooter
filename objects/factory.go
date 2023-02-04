57db9fe9391d36a4e78b7a8fc64407d70a1afb80
AlexSaliev <alexsaliev93@gmail.com>
2023-02-04 17:45:43 +0200

    add images

---
+++ objects/factory.go
@@ -0,0 +1,18 @@
+embedded_components {
+  id: "nokia"
+  type: "factory"
+  data: "prototype: \"\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
