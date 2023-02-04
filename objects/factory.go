0243711a88569f7516bfe721429ca6f60feb72b0
AlexSaliev <alexsaliev93@gmail.com>
2023-02-04 19:34:16 +0200

    add objects and add images

--- objects/factory.go
+++ objects/factory.go
@@ -31,3 +31,39 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "stone"
+  type: "factory"
+  data: "prototype: \"/objects/stone.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "bones"
+  type: "factory"
+  data: "prototype: \"/objects/bones.go\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
