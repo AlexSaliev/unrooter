244679dfd47508b6c6928d8cd3141d59c1582d44
Alexey Khaneft <a.khaneft@gmail.com>
2023-02-04 22:27:29 +0100

    improve config

--- main/constants.lua
+++ main/constants.lua
@@ -1,3 +1,9 @@
 local OBJECT = {}
+OBJECT.CURRENT_AGE = "AGE_1"
+OBJECT.AGES = { 
+	AGE_1 = { next = "AGE_2", minimal_point = 500, damage_object = "#stone", point_object = "#nokia"},
+	AGE_2 = { next = "", minimal_point = 1500, damage_object = "#stone", point_object = "#bones"}
+}
+
 OBJECT.speed = 150
 return OBJECT
