e8164d462b15d61ee7cbb5b6a5cf4731550759b1
AlexSaliev <alexsaliev93@gmail.com>
2023-02-04 15:26:23 +0200

    ground background

--- shovel/shovel.go
+++ shovel/shovel.go
@@ -65,7 +65,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"shovel\"\n"
+  "default_animation: \"Shovel\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
