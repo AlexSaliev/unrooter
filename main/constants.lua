428c97093f6079caaab564b061fc1d9b53510e97
Alexey Khaneft <a.khaneft@gmail.com>
2023-02-05 00:44:13 +0100

    refactor point and damage objects

--- main/constants.lua
+++ main/constants.lua
@@ -4,20 +4,18 @@ OBJECT.AGES = {
 	AGE_1 = { 
 		next = "AGE_2",
 		speed = 150,
-		points = 100,
 		minimal_point = 500, 
-		damage_object = "#stone", 
-		point_object = "#nokia",
+		damage_object = { name = "#stone", hit = 1 }, 
+		point_object = { name = "#nokia", points = 100 },
 		stop_ground = "main:/ground/ground0#ground",
 		start_ground = "main:/ground2/ground0#ground"
 	},
 	AGE_2 = { 
 		next = "", 
 		speed = 250,
-		points = 2000,
 		minimal_point = 1500, 
-		damage_object = "#stone", 
-		point_object = "#bones",
+		damage_object = { name = "#stone", hit = 1 }, 
+		point_object = { name = "#bones", points = 2000 },
 		stop_ground = "",
 		start_ground = ""
 	}
