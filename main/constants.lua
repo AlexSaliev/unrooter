b48929e669010c43d73981bc318d6cc199bed933
AlexSaliev <alexsaliev93@gmail.com>
2023-02-05 11:48:36 +0200

    add  ages 3

--- main/constants.lua
+++ main/constants.lua
@@ -11,9 +11,18 @@ OBJECT.AGES = {
 		start_ground = "main:/ground2/ground0#ground"
 	},
 	AGE_2 = { 
-		next = "", 
+		next = "AGE_3", 
 		speed = 250,
-		minimal_point = 1500, 
+		minimal_point = 5000, 
+		damage_object = { name = "#bomb", hit = 2 }, 
+		point_object = { name = "#bones", points = 2000 },
+		stop_ground = "main:/ground2/ground0#ground",
+		start_ground = "main:/ground3/ground0#ground"
+	},
+	AGE_3 = { 
+		next = "", 
+		speed = 400,
+		minimal_point = 10000, 
 		damage_object = { name = "#bomb", hit = 2 }, 
 		point_object = { name = "#bones", points = 2000 },
 		stop_ground = "",
